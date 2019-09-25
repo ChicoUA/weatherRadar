86b71cd73b581df768450262cc2f859959ce05ca
franciscojesus <franciscojesus@ua.pt>
2019-09-25 17:13:30 +0100

    Logging feature added

--- ies1.2/weatherRadar/src/main/java/ies/aula1/ex1/weatherRadar/App.java
+++ ies1.2/weatherRadar/src/main/java/ies/aula1/ex1/weatherRadar/App.java
@@ -3,11 +3,16 @@ package ies.aula1.ex1.weatherRadar;
 import java.util.Scanner;
 import retrofit2.*;
 import retrofit2.converter.gson.GsonConverterFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class App 
 {
+	private static final Logger LOG = LogManager.getLogger("logger");
+	
     public static void main( String[] args )
     {
+    	LOG.info("Selected Code was "+ args[0]);
     	
     	Retrofit retrofit = new Retrofit.Builder()             
     			 .baseUrl("http://api.ipma.pt/open-data/")             
@@ -17,10 +22,12 @@ public class App
     	 IpmaService service = retrofit.create(IpmaService.class);     
     	 Call<IpmaCityForecast> callSync = service.getForecastForACity(Integer.parseInt(args[0])); 
     	 
-    	 try {         
+    	 try {   
+    		 LOG.info("Getting response from service");
     		 Response<IpmaCityForecast> apiResponse = callSync.execute();         
     		 IpmaCityForecast forecast = apiResponse.body(); 
-    	 
+    		 System.out.println("min temp for today: " + forecast.getData().                 
+    	    		 listIterator().next().getTMin()); 
     	     System.out.println("max temp for today: " + forecast.getData().                 
     	    		 listIterator().next().getTMax()); 
     	 
